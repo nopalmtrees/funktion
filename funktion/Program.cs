c96cb5d4d94a6fccdd2d693bd69a5eefac099a67
Tobbe <tobias.hauptman@student.nbi-handelsakademin.se>
2022-10-20 14:44:35 +0200

    uppdate

--- funktion/Program.cs
+++ funktion/Program.cs
@@ -7,12 +7,18 @@ using System.Threading.Tasks;
 namespace funktion
 {
     internal class Program
+
+
+
     {
+
+        // Deklarerar en global variabel
+        static string carColor;
         static void Main(string[] args)
         {
             Console.WriteLine("Helle world");
             //anropa Myfunction
-            
+
             Myfunction();
             Myfunction();
 
@@ -27,13 +33,26 @@ namespace funktion
             Console.WriteLine(meddelande);
 
             Console.WriteLine(SayHey("Ebbot"));
+
+            //global variabel carcolor
+            Console.WriteLine("Min bil har färgen {0}" + carColor);
+            car();
+            Console.WriteLine(("Min bil har färgen {0}" + carColor));
+
+            // funktion summera två tal
+            int summa = Add(3, 7);
+            summa += Add(x);
+            summa += Add();
+            Console.WriteLine(summa);
+
+              
         }
 
         //skapa en ny funktion som heter myfunctio.
-        static void Myfunction() 
+        static void Myfunction()
         {
             Console.WriteLine("inuti en funktion");
-            
+
         }
 
         //skapa funktonen oneortwo som tar in parametervärde
@@ -42,20 +61,31 @@ namespace funktion
             if (input == 1)
             {
                 Console.WriteLine("input är 1");
-            }else if (input == 2)
+            } else if (input == 2)
             {
                 Console.WriteLine("input är 2");
-               
+
             }
-            
+
         }
         //skapar funktion som returnerar hälsning
         static string SayHey(string name)
         {
             string message = "Hejsan " + name;
             return message;
-            
+
         }
+        static void car()
+        {
+            carColor = "Röd";
+        }
+        //funktion för att returnerna summa av två
+        static int Add(int tal1 =0, int tal2 =0)
+        {
+            return tal1 + tal2; 
+        }
+        //
+        
         
         
         
